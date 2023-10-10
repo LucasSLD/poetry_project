1e5caa1b9b737aae3031a085ba072fbd26eca012
LucasSLD <lucassld700@gmail.com>
2023-10-10 12:07:46 +0200

    feat: reformat

--- poetry_project/flatten_list.py
+++ poetry_project/flatten_list.py
@@ -12,9 +12,8 @@ def flat_list(array: list[int]) -> Iterable[int]:
     """
     l = []
     for e in array:
-        if isinstance(e,list):
+        if isinstance(e, list):
             l += flat_list(e)
         else:
             l.append(e)
     return l
-
