54ed3284d73fe1b138b16c3557471917bb8ceca1
LucasSLD <lucassld700@gmail.com>
2023-10-17 12:45:46 +0200

    refactor: removing napoleon extension

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Lucas'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc','sphinxcontrib.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
