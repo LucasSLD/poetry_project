e417ac917cc43f460d7c1a248361eb2c514ba361
LucasSLD <lucassld700@gmail.com>
2023-10-18 12:29:43 +0200

    fix(poetry): modified sys path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath(".."))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -5,6 +11,8 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
 
 project = 'Poetry project'
 copyright = '2023, Lucas'
